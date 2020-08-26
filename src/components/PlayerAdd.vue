d612c2876210953e31dc4a8de8ad818d207c327f
4kills <dominik.ochs@gmx.net>
2020-08-26 20:08:59 +0200

    added PlayerAdd template

---
+++ src/components/PlayerAdd.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='padder'>
+        <div>
+            <label for='playerName'> {{ $t('playerInput') }}: </label>
+            <input id='playerName' type="text">
+        </div>
+        <div class="marger">
+            <button>{{ $t('playerAdd') }}</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .padder {
+        padding: 20px; 
+    }
+    .marger {
+        margin-top: 10px; 
+    }
+</style>
